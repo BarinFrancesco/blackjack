572f41f02b456593b234fae8e9fdcb09ea20506c
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2025-05-16 21:53:11 +0200

    fine grafica a casa

---
+++ Barin_Vallarsa_Blackjack/Barin_Vallarsa_Blackjack/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Barin_Vallarsa_Blackjack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlFiches10 = new System.Windows.Forms.Panel();
+            this.pnlFiches20 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pnlFiches100 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblPuntata = new System.Windows.Forms.Label();
+            this.pnlFiches500 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlFiches20.SuspendLayout();
+            this.pnlFiches100.SuspendLayout();
+            this.pnlFiches500.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlFiches10
+            // 
+            this.pnlFiches10.BackColor = System.Drawing.Color.Transparent;
+            this.pnlFiches10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlFiches10.BackgroundImage")));
+            this.pnlFiches10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlFiches10.Location = new System.Drawing.Point(31, 560);
+            this.pnlFiches10.Name = "pnlFiches10";
+            this.pnlFiches10.Size = new System.Drawing.Size(50, 80);
+            this.pnlFiches10.TabIndex = 0;
+            this.pnlFiches10.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlFiches10_MouseClick);
+            // 
+            // pnlFiches20
+            // 
+            this.pnlFiches20.BackColor = System.Drawing.Color.Transparent;
+            this.pnlFiches20.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlFiches20.BackgroundImage")));
+            this.pnlFiches20.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlFiches20.Controls.Add(this.panel3);
+            this.pnlFiches20.Location = new System.Drawing.Point(87, 560);
+            this.pnlFiches20.Name = "pnlFiches20";
+            this.pnlFiches20.Size = new System.Drawing.Size(50, 80);
+            this.pnlFiches20.TabIndex = 1;
+            this.pnlFiches20.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlFiches25_MouseClick);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(72, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(75, 75);
+            this.panel3.TabIndex = 2;
+            // 
+            // pnlFiches100
+            // 
+            this.pnlFiches100.BackColor = System.Drawing.Color.Transparent;
+            this.pnlFiches100.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlFiches100.BackgroundImage")));
+            this.pnlFiches100.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlFiches100.Controls.Add(this.panel2);
+            this.pnlFiches100.Location = new System.Drawing.Point(143, 560);
+            this.pnlFiches100.Name = "pnlFiches100";
+            this.pnlFiches100.Size = new System.Drawing.Size(50, 80);
+            this.pnlFiches100.TabIndex = 4;
+            this.pnlFiches100.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlFiches100_MouseClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(72, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(50, 80);
+            this.panel2.TabIndex = 2;
+            // 
+            // lblPuntata
+            // 
+            this.lblPuntata.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuntata.Font = new System.Drawing.Font("Chicago", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntata.ForeColor = System.Drawing.Color.White;
+            this.lblPuntata.Location = new System.Drawing.Point(424, 580);
+            this.lblPuntata.Name = "lblPuntata";
+            this.lblPuntata.Size = new System.Drawing.Size(313, 37);
+            this.lblPuntata.TabIndex = 5;
+            this.lblPuntata.Text = "Puntata: 0$";
+            // 
+            // pnlFiches500
+            // 
+            this.pnlFiches500.BackColor = System.Drawing.Color.Transparent;
+            this.pnlFiches500.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlFiches500.BackgroundImage")));
+            this.pnlFiches500.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlFiches500.Controls.Add(this.panel4);
+            this.pnlFiches500.Location = new System.Drawing.Point(199, 560);
+            this.pnlFiches500.Name = "pnlFiches500";
+            this.pnlFiches500.Size = new System.Drawing.Size(50, 80);
+            this.pnlFiches500.TabIndex = 5;
+            this.pnlFiches500.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlFiches500_MouseClick);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(72, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(75, 75);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(489, 210);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.panel1.Size = new System.Drawing.Size(100, 142);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel5.BackgroundImage")));
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Location = new System.Drawing.Point(535, 210);
+            this.panel5.Margin = new System.Windows.Forms.Padding(0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(100, 142);
+            this.panel5.TabIndex = 7;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Transparent;
+            this.panel6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel6.BackgroundImage")));
+            this.panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel6.Location = new System.Drawing.Point(492, 421);
+            this.panel6.Margin = new System.Windows.Forms.Padding(0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(100, 142);
+            this.panel6.TabIndex = 9;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Transparent;
+            this.panel7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel7.BackgroundImage")));
+            this.panel7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel7.Location = new System.Drawing.Point(446, 421);
+            this.panel7.Name = "panel7";
+            this.panel7.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.panel7.Size = new System.Drawing.Size(100, 142);
+            this.panel7.TabIndex = 8;
+            this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(811, 520);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 10;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.button2.Location = new System.Drawing.Point(925, 520);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(793, 598);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 37);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Annulla puntata";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1173, 694);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlFiches500);
+            this.Controls.Add(this.lblPuntata);
+            this.Controls.Add(this.pnlFiches100);
+            this.Controls.Add(this.pnlFiches20);
+            this.Controls.Add(this.pnlFiches10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlFiches20.ResumeLayout(false);
+            this.pnlFiches100.ResumeLayout(false);
+            this.pnlFiches500.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlFiches10;
+        private System.Windows.Forms.Panel pnlFiches20;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel pnlFiches100;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblPuntata;
+        private System.Windows.Forms.Panel pnlFiches500;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Barin_Vallarsa_Blackjack/Barin_Vallarsa_Blackjack/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barin_Vallarsa_Blackjack
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float puntata = 0;
+        float credito = 1000;
+        private void pnlFiches100_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (credito - puntata - 100 >= 0)
+            {
+                puntata += 100;
+                lblPuntata.Text = $"Puntata: {puntata.ToString()}$";
+            } else
+            {
+                MessageBox.Show("Non hai altri soldi da puntare");
+            }
+
+        }
+
+        private void pnlFiches25_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (credito - puntata - 20 >= 0)
+            {
+                puntata += 20;
+                lblPuntata.Text = $"Puntata: {puntata.ToString()}$";
+            }
+            else
+            {
+                MessageBox.Show("Non hai altri soldi da puntare");
+            }
+        }
+
+        private void pnlFiches10_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (credito - puntata - 10 >= 0)
+            {
+                puntata += 10;
+                lblPuntata.Text = $"Puntata: {puntata.ToString()}$";
+            }
+            else
+            {
+                MessageBox.Show("Non hai altri soldi da puntare");
+            }
+        }
+
+        private void pnlFiches500_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (credito - puntata - 500 >= 0)
+            {
+                puntata += 500;
+                lblPuntata.Text = $"Puntata: {puntata.ToString()}$";
+            }
+            else
+            {
+                MessageBox.Show("Non hai altri soldi da puntare");
+            }
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+            //crea un'immanige con il percorso dato, e la mette all'interno di panel 1, unato questo perché coì non ci sono bordi o margini brutti da vedere
+            Image img = Image.FromFile("../../Images/mazzo/carta-coperta.jpg");
+            e.Graphics.DrawImage(img, panel5.ClientRectangle);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Image img = Image.FromFile("../../Images/mazzo/re-fiori.jpg");
+            e.Graphics.DrawImage(img, panel1.ClientRectangle);
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+            Image img = Image.FromFile("../../Images/mazzo/donna-quadri.jpg");
+            e.Graphics.DrawImage(img, panel6.ClientRectangle);
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+            Image img = Image.FromFile("../../Images/mazzo/asso-picche.jpg");
+            e.Graphics.DrawImage(img, panel7.ClientRectangle);
+        }
+    }
+}
